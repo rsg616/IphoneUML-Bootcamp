3aed6bac7ea54c7b3dbcc21aad4bf3f872e680d5
Nestor Garraza Sondahl <148396273+rsg616@users.noreply.github.com>
2025-05-03 19:28:28 -0300

    Add files via upload

---
+++ Iphone.java
@@ -0,0 +1,46 @@
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    public void tocar() {
+        System.out.println("Tocando música...");
+    }
+
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    public void selecionarMusica(String musica) {
+        System.out.println("Música selecionada: " + musica);
+    }
+
+    public void ligar(String numero) {
+        System.out.println("Ligando para: " + numero);
+    }
+
+    public void atender() {
+        System.out.println("Chamada atendida.");
+    }
+
+    public void iniciarCorreioVoz() {
+        System.out.println("Correio de voz iniciado.");
+    }
+
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+
+    public static void main(String[] args) {
+        Iphone iphone = new Iphone();
+
+        iphone.tocar();
+        iphone.ligar("123456789");
+        iphone.exibirPagina("https://www.apple.com");
+    }
+}
